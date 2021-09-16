669005bb3d57ff40c98688ca7bcbd2483d001186
s.claes <Sadrian2711>
2021-09-16 15:31:06 +0200

    [ADD] defining the ClickMouse class and all upcoming methods

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
+
+
+delay = 10
+button = Button.left
+start_stop_key = KeyCode(char='s')
+exit_key = KeyCode(char='e')
+
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+
+    def start_clicking(self):
+
+    def stop_clicking(self):
+
+    def exit(self):
+
+    def run(self):
