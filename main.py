41f9645574645d9e5773b99c48dca03b659b8934
s.claes <Sadrian2711>
2021-09-16 15:33:30 +0200

    [ADD] defining all attributes of the __init__ method

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ exit_key = KeyCode(char='e')
 
 class ClickMouse(threading.Thread):
     def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
 
     def start_clicking(self):
 
     def stop_clicking(self):
 
     def exit(self):
+
 
     def run(self):
